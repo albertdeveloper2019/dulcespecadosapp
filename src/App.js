86902e6867c5d3195bdf6e612a6fbcde6758542f
albert <amdeveloper2019@gmail.com>
2020-04-29 11:27:22 -0400

    clear react default

--- src/App.js
+++ src/App.js
@@ -4,23 +4,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <div className="App-header">
+              {/* <img src={logo} className="App-logo" alt="logo" /> */}
+              <h2>Bienvenido a Dulces Pecados </h2>
+          </div>
+      </div>
   );
-}
+
+}  // llave de app
 
 export default App;
---
+++ src/components/header/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const image_background = 'urlimage'
+
+export const header = ({ cover = image_background }) => (
+
+    <img src = {cover}>    </img>
+    
+)

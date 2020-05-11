c1d3099953989acd202ddc25e63a512d7b017c73
albert <amdeveloper2019@gmail.com>
2020-05-11 17:46:32 -0400

    contact listo

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,11 +1,32 @@
 import React, {Component} from 'react';
+import img2 from '../../images/logo180.png'
+import Image from 'react-bootstrap/Image'
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import { AiOutlineWhatsApp, AiFillFacebook, AiFillInstagram } from "react-icons/ai";
 
 class Contact extends React.Component {
     render() {
          return (      
-                <div className="App-dashboard">
-                     <h3> Contact </h3>
-                </div>        
+          <div className = "App-div-contact"> 
+          <Grid fliud>
+          <Row>
+              <Col xs={3} className = "App-cell-left">
+                   
+              </Col>
+              <Col xs={2} className = "App-cell-icons" >
+                 <Image src={img2} roundedCircle/>
+                 {/* <FcHome />  <Link to="/" className = "App-Links">Inicio</Link> */}
+              </Col>
+              <Col xs={2} className = "App-social-media-ws" >
+                    <AiOutlineWhatsApp /> +58 4242384314
+              </Col>
+              <Col xs={2} className = "App-social-media-ig" >
+                  <AiFillInstagram />  dulcespecados6
+              </Col>
+          </Row>
+        </Grid>
+        </div>
+
          );
     }
 }
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,27 +1,26 @@
 import React, {Component} from 'react';
 import Logo from '../logo/index';
 import { Grid, Row, Col } from 'react-flexbox-grid';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { FcHome,FcContacts,FcNews } from "react-icons/fc";
+import { FcHome,FcContacts,FcCameraIdentification } from "react-icons/fc";
 import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 class Header extends React.Component {
     render() {
          return (
 
-              <Grid fliud>
+              <Grid>
                 <Row>
-                    <Col xs={3} className = "App-cell-left">
+                    <Col Col xs={12} sm={3} md={2} lg={1} className = "App-cell-left">
                        <Logo/>
                     </Col>
-                    <Col xs={2} className = "App-cell-icons" >
-                       <FcHome />  <Link to="/" className = "App-Links">Home</Link>
+                    <Col Col xs={3} sm={3} md={3} lg={3} className = "App-cell-icons" >
+                       <FcHome />  <Link to="/" className = "App-Links-home">Inicio</Link>
                     </Col>
-                    <Col xs={2} className = "App-cell-icons" >
-                        <FcNews />  <Link to="/News" className = "App-Links">News</Link>
+                    <Col Col xs={3} sm={3} md={3} lg={3} className = "App-cell-icons2" >
+                        <FcCameraIdentification />  <Link to="/News" className = "App-Links">Nuestros Pecados</Link>
                     </Col>
-                    <Col xs={2} className = "App-cell-icons" >
-                        <FcContacts />  <Link to="/Contact" className = "App-Links">Contact</Link>
+                    <Col Col xs={3} sm={3} md={3} lg={3} className = "App-cell-icons3" style={{paddingRight : '17%'}}>
+                        <FcContacts />  <Link to="/Contact" className = "App-Links-contact">Contáctenos</Link>
                     </Col>
                 </Row>
               </Grid>

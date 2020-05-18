26b646c2954d89107f3bf4c93b8148cc4b019ca9
albert <amdeveloper2019@gmail.com>
2020-05-17 23:15:33 -0400

    Page contact Done OK

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -3,30 +3,38 @@ import img2 from '../../images/logo180.png'
 import Image from 'react-bootstrap/Image'
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import { AiOutlineWhatsApp, AiFillFacebook, AiFillInstagram } from "react-icons/ai";
+import Card from 'react-bootstrap/Card'
+import img1 from '../../images/Logo jpg.jpg'
 
 class Contact extends React.Component {
     render() {
-         return (      
-          <div className = "App-div-contact"> 
-          <Grid fliud>
-          <Row>
-              <Col xs={3} className = "App-cell-left">
-                   
-              </Col>
-              <Col xs={2} className = "App-cell-icons" >
-                 <Image src={img2} roundedCircle/>
-                 {/* <FcHome />  <Link to="/" className = "App-Links">Inicio</Link> */}
-              </Col>
-              <Col xs={2} className = "App-social-media-ws" >
-                    <AiOutlineWhatsApp /> +58 4242384314
-              </Col>
-              <Col xs={2} className = "App-social-media-ig" >
-                  <AiFillInstagram />  dulcespecados6
-              </Col>
-          </Row>
-        </Grid>
-        </div>
+        return (     
+          <Grid>
+            <Row>
+            <Col Col xs={2} sm={6} md={6} lg={11}>
+                  <div className = "App-div-contact fade-in"> 
 
+                        <Card border="secondary" style={{ width: '20rem'}}>
+                        <Card.Img variant="top" src={img1}  style={{ width: '69%', marginLeft: '13%'}} />
+                        <Card.Body style={{ backgroundColor: 'magenta'}}>
+                            <Card.Title>Whatsapp</Card.Title>
+                            <Card.Text style={{ WebkitTextStrokeWidth: 'thick' }}  >
+                                +58 4242384314
+                            </Card.Text>
+                            <Card.Title>Instagram</Card.Title>
+                            <Card.Text style={{ WebkitTextStrokeWidth: 'thick' }} >
+                                @dulcespecados6
+                            </Card.Text>
+                            <Card.Title>Facebook</Card.Title>
+                            <Card.Text style={{ WebkitTextStrokeWidth: 'thick' }} >
+                                dulcespecados6
+                            </Card.Text>
+                        </Card.Body>
+                        </Card>   
+                  </div>
+                  </Col>
+            </Row>   
+        </Grid>
          );
     }
 }
